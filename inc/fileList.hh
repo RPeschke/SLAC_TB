de0e852d428e23478f96654284cf91891b759f7b
Richard Peschke <Richard.Peschke@desy.de>
2014-08-26 17:23:38 +0200

    removed files from the list since there the correlation was not stable over time or not there at all

--- inc/fileList.hh
+++ inc/fileList.hh
@@ -13,7 +13,7 @@ public:
   
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_10_last_10.root", "C:/slac_data/TelescopeFiles1/Telescope001232_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_15_last_15.root", "C:/slac_data/TelescopeFiles1/Telescope001233_x.root");
-    setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_20_last_20.root", "C:/slac_data/TelescopeFiles1/Telescope001234_x.root");
+    //setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_20_last_20.root", "C:/slac_data/TelescopeFiles1/Telescope001234_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_25_last_25.root", "C:/slac_data/TelescopeFiles1/Telescope001235_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_30_last_30.root", "C:/slac_data/TelescopeFiles1/Telescope001236_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_35_last_35.root", "C:/slac_data/TelescopeFiles1/Telescope001237_x.root");
@@ -28,12 +28,12 @@ public:
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_80_last_80.root", "C:/slac_data/TelescopeFiles1/Telescope001246_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_85_last_85.root", "C:/slac_data/TelescopeFiles1/Telescope001247_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_90_last_90.root", "C:/slac_data/TelescopeFiles1/Telescope001248_x.root");
-    setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_95_last_95.root", "C:/slac_data/TelescopeFiles1/Telescope001249_x.root");
+    //setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_95_last_95.root", "C:/slac_data/TelescopeFiles1/Telescope001249_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_100_last_100.root", "C:/slac_data/TelescopeFiles1/Telescope001250_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_105_last_105.root", "C:/slac_data/TelescopeFiles1/Telescope001251_x.root");
 
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_110_last_110.root", "C:/slac_data/TelescopeFiles1/Telescope001252_x.root");
-    setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_115_last_115.root", "C:/slac_data/TelescopeFiles1/Telescope001253_x.root");
+    //setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_115_last_115.root", "C:/slac_data/TelescopeFiles1/Telescope001253_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_120_last_120.root", "C:/slac_data/TelescopeFiles1/Telescope001254_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_125_last_125.root", "C:/slac_data/TelescopeFiles1/Telescope001255_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_130_last_130.root", "C:/slac_data/TelescopeFiles1/Telescope001256_x.root");
@@ -44,9 +44,9 @@ public:
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_155_last_155.root", "C:/slac_data/TelescopeFiles1/Telescope001261_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_352_start_160_last_160.root", "C:/slac_data/TelescopeFiles1/Telescope001262_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_165_last_165.root", "C:/slac_data/TelescopeFiles1/Telescope001263_x.root");
-    setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_170_last_170.root", "C:/slac_data/TelescopeFiles1/Telescope001264_x.root");
+    //setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_170_last_170.root", "C:/slac_data/TelescopeFiles1/Telescope001264_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_175_last_175.root", "C:/slac_data/TelescopeFiles1/Telescope001265_x.root");
-    setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_180_last_180.root", "C:/slac_data/TelescopeFiles1/Telescope001266_x.root");
+    //setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_180_last_180.root", "C:/slac_data/TelescopeFiles1/Telescope001266_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_185_last_185.root", "C:/slac_data/TelescopeFiles1/Telescope001267_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_190_last_190.root", "C:/slac_data/TelescopeFiles1/Telescope001268_x.root");
     setfiles("C:/slac_data/abc/run_0712_1243/fdata_Run_353_start_195_last_195.root", "C:/slac_data/TelescopeFiles1/Telescope001269_x.root");
